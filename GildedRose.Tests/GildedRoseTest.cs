a5b379a0a40d2f52dfc0ab17dceabab63eabf049
Mike Walker <michael.walker@softwire.com>
2020-09-07 09:57:36 +0100

    Add the first couple of unit tests.

---
+++ GildedRose.Tests/GildedRoseTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    public class GildedRoseTests
+    {
+        [Test]
+        public void RegularItemsDecreaseTheirSellInValueByOneEachDay()
+        {
+            var item = new Item { Name = "Test Item", Quality = 10, SellIn = 5};
+            var gildedRose = new GildedRose(new List<Item> { item });
+            
+            gildedRose.UpdateQuality();
+
+            item.SellIn.Should().Be(4);
+        }
+
+        [Test]
+        public void SulfurasNeverDecreaseTheirSellInValue()
+        {
+            var item = new Item { Name = "Sulfuras, Hand of Ragnaros", Quality = 10, SellIn = 5};
+            var gildedRose = new GildedRose(new List<Item> { item });
+            
+            gildedRose.UpdateQuality();
+
+            item.SellIn.Should().Be(5);
+        }
+    }
+}
